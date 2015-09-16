7d5ed9c1755c67734b29e2e1498ba1ec36055b17
tkmagesh <tkmagesh77@gmail.com>
2015-09-16 16:15:29 +0530

    Closures - Part - 1

--- Day-01/02-language/src/calculator.js
+++ Day-01/02-language/src/calculator.js
@@ -1,16 +1,8 @@
 function add(x,y){
     function parseArg(n){
-        if (n instanceof Array){
-            var result = 0;
-            for(var i=0; i<n.length; i++)
-                result += parseArg(n[i]);
-            return result;
-        }
+        if (n instanceof Array) return add.apply(this, n);
         if (typeof n === 'function') return parseArg(n());
         return isNaN(n) ? 0 : parseInt(n,10);
     }
-    var result = 0;
-    for(var i=0; i<arguments.length; i++)
-        result += parseArg(arguments[i]);
-    return result;
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
---
+++ Day-01/03-Closures/spinner-assignment.js
@@ -0,0 +1,16 @@
+/*
+Create an object and assign it to variable "spinner"
+
+the object is should exhibit the following behavior
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+
+spinner.down() // => 3
+spinner.down() // => 2
+spinner.down() // => 1
+spinner.down() // => 0
+spinner.down() // => -1
+*/
