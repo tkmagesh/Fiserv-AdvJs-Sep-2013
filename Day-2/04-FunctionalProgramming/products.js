854e40469ef49a1e33ba51d782b133d11e6220b0
tkmagesh <tkmagesh77@gmail.com>
2015-09-17 12:37:01 +0530

    Sort - Solution

--- Day-2/04-FunctionalProgramming/products.js
+++ Day-2/04-FunctionalProgramming/products.js
@@ -44,19 +44,43 @@ describe("Sort", function(){
         console.table(products);
     });
     describe("Generic Sort [ Any list by any attribute ]", function(){
-        function sort(){
-            for(var i=0; i<products.length-1; i++)
-                for(var j=i+1; j<products.length; j++)
-                    if (products[i].id > products[j].id){
-                        var temp = products[i];
-                        products[i] = products[j];
-                        products[j] = temp;
+        function sort(list, attrName){
+            for(var i=0; i<list.length-1; i++)
+                for(var j=i+1; j<list.length; j++)
+                    if (list[i][attrName] > list[j][attrName]){
+                        var temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
                     }
         }
         describe("Products by cost", function(){
+            sort(products, "cost");
             console.table(products);
         });
         describe("Products by units", function(){
+            sort(products, "units");
+            console.table(products);
+        });
+    });
+    describe("Generic Sort [ Any list by any comparer ]", function(){
+        function sort(list, comparerFn){
+            for(var i=0; i<list.length-1; i++)
+                for(var j=i+1; j<list.length; j++)
+                    if (comparerFn(list[i], list[j]) > 0){
+                        var temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+        }
+        describe("Products by cost", function(){
+            var productComparerByValue = function(p1, p2){
+                var p1Value = p1.cost * p1.units,
+                    p2Value = p2.cost * p2.units;
+                if (p1Value > p2Value) return 1;
+                if (p1Value < p2Value) return -1;
+                return 0;
+            }
+            sort(products, productComparerByValue);
             console.table(products);
         });
     });
